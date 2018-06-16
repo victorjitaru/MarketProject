211a89d60ef801f5d59f20fa2b32c2137eee3ae8
Victor Jitaru <jitaruvictor@gmail.com>
2018-06-16 16:00:27 +0300

    Added Product Views

---
+++ src/main/java/ro/sda/eshop/view/ProductView.java
@@ -0,0 +1,9 @@
+package ro.sda.eshop.view;
+
+import ro.sda.eshop.model.Product;
+
+public class ProductView {
+    public void displayDetailedProduct(Product product){
+        product.toString();
+    }
+}
