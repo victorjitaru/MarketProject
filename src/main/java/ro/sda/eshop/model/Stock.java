3179df0701940f7eab912f86dc24894569996a09
Emanuel Blaj <emanuelblaj93@gmail.com>
2018-06-16 10:40:41 +0300

    updated stock model

--- src/main/java/ro/sda/eshop/model/Stock.java
+++ src/main/java/ro/sda/eshop/model/Stock.java
@@ -1,4 +1,42 @@
 package ro.sda.eshop.model;
 
 public class Stock {
+    private Long id;
+    private Long productId;
+    private String location;
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+
 }
