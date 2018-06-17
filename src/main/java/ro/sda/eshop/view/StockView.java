ec0618938b8f35316a12b4113256aced6c350e7b
Dorin Dobri <dobridorin@gmail.com>
2018-06-17 14:56:25 +0300

    Workt on Stok View

--- src/main/java/ro/sda/eshop/view/StockView.java
+++ src/main/java/ro/sda/eshop/view/StockView.java
@@ -1,14 +1,21 @@
 package ro.sda.eshop.view;
 
+import ro.sda.eshop.model.Order;
+import ro.sda.eshop.reader.OrderReader;
+import ro.sda.eshop.reader.impl.OrderReaderImpl;
 import ro.sda.eshop.repository.impl.StockHolder;
+import ro.sda.eshop.service.OrderService;
+
 import java.util.Scanner;
 
 public class StockView implements MarketViewInterface {
     private static final int EXIT_OPTION = 0;
+
+    OrderService orderService = new OrderService();
     @Override
     public void displayView() {
         int option = -1;
-        System.out.println("1. List Stock.");
+        System.out.println("1. List Stocks.");
         System.out.println("2. Add stock.");
         System.out.println("0. Exit");
         option = readOption();
@@ -32,8 +39,9 @@ public class StockView implements MarketViewInterface {
 
                 return;
             case 2:
-                StockHolder stockHolder= new StockHolder();
-               // stockHolder.addStock();
+                OrderReader orderReader = new OrderReaderImpl();
+                Order order = orderReader.readOrder();
+                orderService.placeOrder(order);
                 return;
             case 0:
             default:
